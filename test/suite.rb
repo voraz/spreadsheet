ed18d042ce0b2afc45951c48675835cf0696cb93
Roel van der Hoorn <vanderhoorn@gmail.com>
2011-07-20 15:10:30 +0200

    Fix suite.rb so it no longer uses look behind.

--- test/suite.rb
+++ test/suite.rb
@@ -2,13 +2,14 @@
 # suite.rb -- oddb -- 08.01.2009 -- hwyss@ywesee.com
 
 require 'find'
+require 'rubygems'
 
 here = File.dirname(__FILE__)
 
 $: << here
 
 Find.find(here) do |file|
-	if /(?<!suite)\.rb$/o.match(file)
+	if /[^suite]\.rb$/o.match(file)
     require file
 	end
 end
